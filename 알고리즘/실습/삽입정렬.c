4a6ee8c9850e5898b00f38b56ca6c9ba42385aa2
mango <brightdev.bs@gmail.com>
2021-05-04 17:46:35 +0900

    삽입정렬과 퀵정렬

---
+++ 알고리즘/실습/삽입정렬.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int i, j, tmp;
+	int arr[10] = {1,10,5,8,7,6,4,3,2,9};
+	for(i = 0; i< 9; i++) {
+		j = i;
+		while(j >= 0 && arr[j] > arr[j+1]) {
+			tmp = arr[j];
+			arr[j] = arr[j+1];
+			arr[j+1] = tmp;
+			j--;
+		}
+	}
+	for(i = 0; i < 10; i++) {
+		printf("%d ", arr[i]);
+	}
+}
