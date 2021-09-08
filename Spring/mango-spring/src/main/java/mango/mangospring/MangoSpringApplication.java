0de9576fc844c86a2d722859c426fd692edbc752
mango <brightdev.bs@gmail.com>
2021-09-08 22:31:34 +0900

    스프링 공부 시작

---
+++ Spring/mango-spring/src/main/java/mango/mangospring/MangoSpringApplication.java
@@ -0,0 +1,13 @@
+package mango.mangospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoSpringApplication.class, args);
+	}
+
+}
---
+++ Spring/mango-spring/src/test/java/mango/mangospring/MangoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mango.mangospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
