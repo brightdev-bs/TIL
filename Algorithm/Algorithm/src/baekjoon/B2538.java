a9bb3ade2fedb18ca9d3cf1554577575107bc1d2
mango <brightdev.bs@gmail.com>
2021-09-14 04:05:42 +0900

    백준 2538번

---
+++ Algorithm/Algorithm/src/baekjoon/B2538.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+// 꼭 풀어보기 너무 어려움.
+public class B2538 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int e = sc.nextInt();
+        for(int i = 0; i < e; i++) {
+
+        }
+    }
+}
