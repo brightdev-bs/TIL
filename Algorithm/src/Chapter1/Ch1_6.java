df945575bd4eddd142a2e928c484e1eb70393cde
mango <brightdev.bs@gmail.com>
2021-09-21 14:55:19 +0900

    폴더 정리

---
+++ Algorithm/src/Chapter1/Ch1_6.java
@@ -0,0 +1,24 @@
+package Chapter1;
+
+import java.util.Scanner;
+
+public class Ch1_6 {
+    public static int[] ch = new int[26];
+
+    public static void main(String[] agrs) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String answer = "";
+
+        for(char c : s.toCharArray()) {
+            int index = c - 'a';
+            if(ch[index] == 0) {
+                ch[index] = 1;
+                answer += ch[index];
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
