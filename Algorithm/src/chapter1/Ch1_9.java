ed502fbc02fe3ea6016ff40bcdb2051b4ae68863
mango <brightdev.bs@gmail.com>
2021-09-22 16:39:46 +0900

    1장 알고리즘 문제풀이 완료

---
+++ Algorithm/src/chapter1/Ch1_9.java
@@ -0,0 +1,27 @@
+package chapter1;
+
+import java.util.Scanner;
+
+public class Ch1_9 {
+
+    public static int solution(String s) {
+        String answer = "";
+
+        for(int i = 0; i < s.length(); i++) {
+            if(Character.isDigit(s.charAt(i))) {
+                answer += s.charAt(i);
+            }
+        }
+
+
+        return Integer.parseInt(answer);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        System.out.println(solution(s));
+    }
+}
