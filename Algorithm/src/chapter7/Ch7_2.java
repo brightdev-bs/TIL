385e0d61c2cc31ba910de5cd048b3788139711cb
mango <brightdev.bs@gmail.com>
2021-10-03 17:17:26 +0900

    7장 1~6 공부

---
+++ Algorithm/src/chapter7/Ch7_2.java
@@ -0,0 +1,22 @@
+package chapter7;
+
+import java.util.Scanner;
+
+public class Ch7_2 {
+
+    public static void solution(int n) {
+        if(n == 0) return;
+        else {
+            solution(n / 2);
+            System.out.print(n % 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        solution(n);
+    }
+}
---
+++ Algorithm/src/chapter7/Ch7_4.java
@@ -0,0 +1,25 @@
+package chapter7;
+
+import java.util.Scanner;
+
+public class Ch7_4 {
+    public static int[] memo;
+
+    public static int solution(int n) {
+        if(memo[n] != 0) return memo[n];
+        if(n == 1 || n == 2) return memo[n] = 1;
+        else {
+            return memo[n] = solution(n-2) + solution(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        memo = new int[n+1];
+
+        for(int i = 1; i <= n; i++)
+            System.out.println(solution(i));
+    }
+}
