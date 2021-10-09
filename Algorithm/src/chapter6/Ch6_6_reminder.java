3d98f87b1e725a9c81c20ebe68d72a0d6300c67d
mango <brightdev.bs@gmail.com>
2021-10-09 17:15:34 +0900

    6장 복습

---
+++ Algorithm/src/chapter6/Ch6_6_reminder.java
@@ -0,0 +1,30 @@
+package chapter6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Ch6_6_reminder {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] tmp = arr.clone();
+        Arrays.sort(tmp);
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] != tmp[i]) System.out.print(i+1 + " ");
+        }
+
+
+    }
+}
---
+++ Algorithm/src/chapter6/Ch6_9_again.java
@@ -0,0 +1,8 @@
+package chapter6;
+
+public class Ch6_9_again {
+
+    public static void main(String[] args) {
+
+    }
+}
