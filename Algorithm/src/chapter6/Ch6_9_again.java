f172286372331c48220c3a80112fe12cf4337abe
mango <brightdev.bs@gmail.com>
2021-10-12 23:17:46 +0900

    폴더 삭제

--- Algorithm/src/chapter6/Ch6_9_again.java
+++ Algorithm/src/chapter6/Ch6_9_again.java
@@ -1,8 +1,10 @@
 package chapter6;
 
+import java.io.OutputStreamWriter;
+
 public class Ch6_9_again {
 
     public static void main(String[] args) {
-
+        OutputStreamWriter osw = new OutputStreamWriter(System.in, "UTF-8");
     }
 }
