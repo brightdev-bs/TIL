3738db978db83178ce1d73387577dc6031566a51
Byeongsu KIM <brightdev.bs@gmail.com>
2022-01-03 23:34:49 +0900

    DFS알고리즘

--- Algorithm/src/StringPrac.java
+++ Algorithm/src/StringPrac.java
@@ -1,12 +1,28 @@
+import java.util.*;
+
 public class StringPrac {
 
     public static void main(String[] args) {
-        String s = "ORG 0 //It is just a practice";
-        System.out.println(s.indexOf("//"));
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("1", 1); map.put("2", 2); map.put("3", 3);
 
-        String answer = s.substring(0,6);
-        String comment = s.substring(6);
-        System.out.println(answer);
-        System.out.println(comment);
+        System.out.println("EntrySet을 이용한 방법");
+
+        Set set = map.entrySet();
+        Iterator it = set.iterator();
+        while(it.hasNext()) {
+            Map.Entry e = (Map.Entry) it.next();
+            System.out.println("e.getKey() = " + e.getKey() + " e.getValue() = " + e.getValue());
+        }
+
+        System.out.println("keySet를 이용한 방법");
+
+        Set keys = map.keySet();
+        System.out.println("keys = " + keys);
+
+        Collection values = map.values();
+        System.out.println("values = " + values);
+
+
     }
 }
