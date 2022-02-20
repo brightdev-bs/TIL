53f7f7a14fb639b622741e3d2fb80ece7ab2887a
mango <brightdev.bs@gmail.com>
2022-02-20 18:49:31 +0900

    알고리즘 공부 복습 완료

---
+++ Algorithm/src/chapter10/P_6.java
@@ -0,0 +1,30 @@
+package chapter10;
+
+import java.util.Scanner;
+
+public class P_6 {
+
+    static class Question {
+        int score, time;
+
+        public Question(int score, int time) {
+            this.score = score;
+            this.time = time;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+
+        Question[] arr = new Question[n];
+        for(int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            arr[i] = new Question(a, b);
+        }
+
+    }
+}
