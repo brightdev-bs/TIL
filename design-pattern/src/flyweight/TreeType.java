69fba0ac93916bd9a2fe2406a34044bab7797e8e
Byeongsu KIM <brightdev.bs@gmail.com>
2023-02-25 00:12:07 +0900

    디자인 패턴 - 플라이웨이패턴

---
+++ design-pattern/src/flyweight/TreeType.java
@@ -0,0 +1,16 @@
+package flyweight;
+
+import java.awt.*;
+
+public class TreeType {
+
+    private String name;
+    private Color color;
+
+    public TreeType() {}
+    public TreeType(String name, Color color) {
+        this.name = name;
+        this.color = color;
+    }
+
+}
