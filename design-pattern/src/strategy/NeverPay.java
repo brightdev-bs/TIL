7707830c9fa6447643ba0ee96c031e7212085b01
Byeongsu KIM <brightdev.bs@gmail.com>
2023-03-04 20:10:32 +0900

    디자인 패턴  - 전략 패턴

---
+++ design-pattern/src/strategy/NeverPay.java
@@ -0,0 +1,18 @@
+package strategy;
+
+public class NeverPay implements PayStrategy {
+
+    private String id;
+    private String password;
+
+    public NeverPay(String id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+
+    @Override
+    public void pay(int amount) {
+        System.out.println("네버페이로 " + amount + " 결제하였습니다.");
+
+    }
+}
---
+++ design-pattern/src/strategy/PayStrategy.java
@@ -0,0 +1,7 @@
+package strategy;
+
+public interface PayStrategy {
+
+    void pay(int amount);
+
+}
