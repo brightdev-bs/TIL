366ce919550dd39334a6960360b31e9efc689033
Byeongsu KIM <brightdev.bs@gmail.com>
2023-04-26 01:29:05 +0900

    테스트 주도 개발  - 5장 : 솔직히 말하자면     -> 최대한 빠르게 실행 가능한 코드로 만든다. 이 단계까지는 코드의 중복이 생겨도 상관없다

---
+++ tdd/src/test/java/com/example/tdd/ch1/Franc.java
@@ -0,0 +1,19 @@
+package com.example.tdd.ch1;
+
+public class Franc {
+
+    private int amount;
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Franc franc = (Franc) obj;
+        return amount == franc.amount;
+    }
+}
