214aae3d48833ebfc17329c0c1918ef71e6241a7
Byeongsu KIM <brightdev.bs@gmail.com>
2023-04-26 01:35:18 +0900

    테스트 주도 개발  - 6장 : 돌아온 '모두를 위한 평등'     -> 중복 코드를 제거한 부분을 확인하자

--- tdd/src/test/java/com/example/tdd/ch1/Franc.java
+++ tdd/src/test/java/com/example/tdd/ch1/Franc.java
@@ -1,19 +1,12 @@
 package com.example.tdd.ch1;
 
-public class Franc {
+public class Franc extends Money {
 
-    private int amount;
     public Franc(int amount) {
         this.amount = amount;
     }
 
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        Franc franc = (Franc) obj;
-        return amount == franc.amount;
     }
 }
---
+++ tdd/src/test/java/com/example/tdd/ch1/Money.java
@@ -0,0 +1,12 @@
+package com.example.tdd.ch1;
+
+public class Money {
+
+    protected int amount;
+
+    @Override
+    public boolean equals(Object obj) {
+        Money money = (Money) obj;
+        return amount == money.amount;
+    }
+}
