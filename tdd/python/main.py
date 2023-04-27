061cf9e4b44ce045b12db2e741e553cbef4c6d88
Byeongsu KIM <brightdev.bs@gmail.com>
2023-04-27 23:42:12 +0900

    테스트 주도 개발   - 21장 : 셈하기

--- tdd/python/main.py
+++ tdd/python/main.py
@@ -2,10 +2,13 @@ class TestCase:
     def __init__(self, name):
         self.name=name
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name);
         method()
         self.tearDown()
+        return result
 
     def setUp(self):
         pass
@@ -21,6 +24,8 @@ class WasRun(TestCase):
         self.log = "setUp "
     def tearDown(self):
         self.log = self.log + "tearDown"
+    def testBrokenMethod(self):
+        raise Exception
 class TestCaseTest(TestCase) :
     def setUp(self):
         self.test = WasRun("testMethod")
@@ -28,6 +33,21 @@ class TestCaseTest(TestCase) :
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown" == test.log)
+    def testResult(self):
+        test = WasRun("TestMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result= test.run()
+        assert("1 run, 1 failed" == result.summary())
 
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount= self.runCount + 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
 
 TestCaseTest("testTemplateMethod").run()
