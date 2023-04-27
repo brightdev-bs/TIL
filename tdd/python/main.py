98a5ca47bb2b57efeb98dbd7428efe176da1e26b
Byeongsu KIM <brightdev.bs@gmail.com>
2023-04-27 22:23:51 +0900

    테스트 주도 개발   - 20장 : 뒷정리하기

--- tdd/python/main.py
+++ tdd/python/main.py
@@ -1,36 +1,33 @@
 class TestCase:
     def __init__(self, name):
         self.name=name
-    def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
     def run(self):
         self.setUp()
         method = getattr(self, self.name);
         method()
+        self.tearDown()
 
-    def testSetUp(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert(test.wasSetUp)
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun=None;
-        TestCase.__init__(self, name)
     def testMethod(self):
         self.wasRun=1
+        self.log= self.log + "testMethod "
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp= 1
-
+        self.log = "setUp "
+    def tearDown(self):
+        self.log = self.log + "tearDown"
 class TestCaseTest(TestCase) :
     def setUp(self):
         self.test = WasRun("testMethod")
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown" == test.log)
 
 
-TestCaseTest("testRunning").run();
+TestCaseTest("testTemplateMethod").run()
