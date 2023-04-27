3a2e19aaf0fe521bb85ae9c912f3906aca6687ce
Byeongsu KIM <brightdev.bs@gmail.com>
2023-04-27 21:43:45 +0900

    테스트 주도 개발   - 19장 : 테이블 차리기

--- tdd/python/main.py
+++ tdd/python/main.py
@@ -1,21 +1,36 @@
 class TestCase:
     def __init__(self, name):
         self.name=name
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
     def run(self):
+        self.setUp()
         method = getattr(self, self.name);
         method()
+
+    def testSetUp(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert(test.wasSetUp)
 class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun=None;
         TestCase.__init__(self, name)
     def testMethod(self):
         self.wasRun=1
+    def setUp(self):
+        self.wasSetUp= 1
 
 class TestCaseTest(TestCase) :
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
 
 TestCaseTest("testRunning").run();
