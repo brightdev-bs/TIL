3337350bbe5dfe32a05c90d2da5468f7af7fb9ab
Byeongsu KIM <brightdev.bs@gmail.com>
2023-05-13 17:08:25 +0200

    디자인 패턴 - 옵저버 패턴  - 연습

---
+++ design-pattern/src/headfirst/observer/forecast/ForecastDisplay.java
@@ -0,0 +1,29 @@
+package headfirst.observer.forecast;
+
+import headfirst.observer.DisplayElement;
+import headfirst.observer.Observer;
+import headfirst.observer.WeatherData;
+
+public class ForecastDisplay implements Observer, DisplayElement {
+
+    private float currentPressure = 29.92f;
+    private float lastPressure;
+    private WeatherData weatherData;
+
+    public ForecastDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        // 디스플레이 코드
+    }
+
+    @Override
+    public void update() {
+        lastPressure = currentPressure;
+        currentPressure = weatherData.getPressure();
+        display();
+    }
+}
