b24bb3f9b283528ac979612971054c59c342ac1c
Byeongsu KIM <brightdev.bs@gmail.com>
2023-06-03 14:18:27 +0200

    자바 웹 프로그래밍 Next Step - 2장 리팩토링

--- NextStep/src/Calculator.java
+++ NextStep/src/Calculator.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calculator {
 
     public int add(int a, int b) {
@@ -6,5 +9,48 @@ public class Calculator {
 
     public int subtract(int a, int b) {
         return a - b;
+    }
+
+    public int splitAndGetSum(String s) {
+        if(s == null || s.isBlank()) return 0;
+        return getSum(toInts(splitString(s)));
+    }
+
+    private int getSum(int[] nums) {
+        int sum = 0;
+        for (int n : nums) {
+            int num = n;
+            if(num < 0) throw new RuntimeException();
+            sum += num;
+        }
+        return sum;
+    }
+
+    private int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            numbers[i] = toPositive(values[i]);
+        }
+        return numbers;
+    }
+
+    private static int toPositive(String values) {
+        int parseInt = Integer.parseInt(values);
+        if(parseInt < 0) {
+            throw new RuntimeException();
+        }
+        return parseInt;
+    }
+
+    private String[] splitString(String s) {
+        int sum;
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(s);
+
+        if(m.find()) {
+            String customDelimeter = m.group(1);
+            return m.group(2).split(customDelimeter);
+        }
+
+        return s.split(",|:");
     }
 }
--- NextStep/test/CalculatorTest.java
+++ NextStep/test/CalculatorTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
 
@@ -18,5 +19,29 @@ public class CalculatorTest {
     @Test
     public void subtract() {
         assertEquals(3, cal.subtract(6, 3));
+    }
+
+    // 쉼표 또는 콜론을 구분자로 각 숫자의 합을 반환한다.
+    @Test
+    public void splitAndAdd() {
+        assertEquals(0, cal.splitAndGetSum(null));
+        assertEquals(0, cal.splitAndGetSum(" "));
+        assertEquals(1, cal.splitAndGetSum("1"));
+        assertEquals(3, cal.splitAndGetSum("1,2"));
+        assertEquals(6, cal.splitAndGetSum("1,2,3"));
+        assertEquals(6, cal.splitAndGetSum("1,2:3"));
+    }
+
+    // 커스텀 구분자를 지정할 수 있다.
+    @Test
+    public void customAdd() {
+        assertEquals(6, cal.splitAndGetSum("//;\n1;2;3"));
+    }
+
+
+    // 문자열 계산기에 음수를 전달하는 경우 RuntimeException으로 예외 처리한다.
+    @Test
+    public void minusNumbersRuntimeException() {
+        assertThrows(RuntimeException.class, () -> cal.splitAndGetSum("-1,2"));
     }
 }
